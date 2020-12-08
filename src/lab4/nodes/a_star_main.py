c84dd7192a2f9d1564a17b049e82042a6819735f
Mohamed Fazil <mohamedfazilsulaiman@gmail.com>
2020-12-07 20:04:36 -0500

    Updated the COMMENTS and cleaned the code

--- src/lab4/nodes/a_star_main.py
+++ src/lab4/nodes/a_star_main.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
+'''
+  ROS A star Controll Node
+  ------------------------
+  Created by Mohamed Fazil on 12/7/20.
+
+  email: mohamedfazilsulaiman@gmail.com
+  github: https://github.com/fazildgr8
+'''
 import roslib
 roslib.load_manifest('lab4')
 import rospy
@@ -14,9 +22,13 @@ from visualization_msgs.msg import Marker
 from tf.transformations import euler_from_quaternion
 import numpy as np
 import time
+import sys
 from pprint import pprint
 ################### Classes #####################################
 class Node:
+    '''
+    Class which stores the node location,parent and costs
+    '''
     def __init__(self,position):
         self.location = position
         self.prev = None
@@ -34,6 +46,7 @@ class Node:
 ################### Global Variables ####################################
 global robot_location, robot_rotation, final_goal_location, robot_start_location, global_map, final_path, map_odom_trans, goal_reached
 
+# Load the Map as an Array 0 - Empty, 1 - Occupied
 global_map = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0,0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0,0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0],
@@ -55,6 +68,7 @@ global_map = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0,0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1,1, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1,1, 1]])
 
+
 print('Loaded Map of Shpae - ',global_map.shape)
 final_path = None
 laser_ranges = np.zeros((361,))
@@ -64,19 +78,15 @@ robot_location = robot_start_location
 robot_rotation = [0,0,0]
 goal_reached = False
 iter_break = False
-# rospy.set_param('goalx', -8)
-# rospy.set_param('goaly', 8)
 
-# rospy.set_param('goalx', 3)
-# rospy.set_param('goaly', -9)
-
-# rospy.set_param('goalx', 4.5)
-# rospy.set_param('goaly', 9)
 
 final_goal_location = [rospy.get_param('/goalx'), rospy.get_param('/goaly'), 0]
 ##################### Perception Part #####################
 
 def goal_location_marker(final_goal_location):
+    '''
+    Publishes the Goal Location Text in RVIZ
+    '''
     marker_pub = rospy.Publisher('goal_location_marker', Marker,queue_size=1) # Publish Robot Position to RVIZ
     marker_data = Marker()
     marker_data.type = marker_data.TEXT_VIEW_FACING
@@ -102,11 +112,17 @@ def goal_location_marker(final_goal_location):
     marker_pub.publish(marker_data)
 
 def callback_laser(msg):
+    '''
+    Laser Msgs
+    '''
     global laser_ranges, laser_intensities
     laser_ranges = np.array(msg.ranges)
     laser_intensities = np.array(msg.intensities)
 
 def callback_odom(msg):
+    '''
+    Obtains Odometer readings and assigns to global Variables
+    '''
     global robot_location, robot_rotation, robot_orientation, global_map
     location = [msg.pose.pose.position.x, msg.pose.pose.position.y]
     robot_location = location
@@ -118,9 +134,13 @@ def callback_odom(msg):
     robot_cube_publisher(location,robot_orientation)
     map_g = np.copy(global_map)
     map_g[13][8] = 0
-    OccupancyGrid_publish(map_g[::-1])
+    OccupancyGrid_publish(map_g[::-1]) # Publish the Occupancy Grid
+    goal_location_marker(final_goal_location) # Publish Goal 
 
 def robot_cube_publisher(trans,rot):
+    '''
+    Publishes Robot Marker in RVIZ
+    '''
     marker_pub = rospy.Publisher('robot_marker', Marker,queue_size=1) # Publish Robot Position to RVIZ
     marker_data = Marker()
     marker_data.type = marker_data.CUBE
@@ -142,6 +162,9 @@ def robot_cube_publisher(trans,rot):
     marker_pub.publish(marker_data)
 
 def OccupancyGrid_publish(global_map):
+    '''
+    Publishes Occcupancy Grid in RVIZ
+    '''
     data = np.array(100*global_map,dtype=np.int8)
     map_msg = OccupancyGrid()
     map_msg.header.frame_id = '/odom'
@@ -181,22 +204,42 @@ def points_publisher(points_list):
 
 def A_STAR(global_map, start, end, Type = '8c',e = 1, heuristic = 'eu' ):
     """
+    Implements A* Algorithm for the Given Grid Map
+    Arguments:
+    global_map - np.array()
+    start - start location [x,y]
+    end - end location [x,y]
+    Type - Type of Neibhours ('8c' or '4c')
+    e - Heurestic Factor
+    heuristic - Distance Method ('d' - distance, 'eu' - euclidean distance, 'manhattan' - manhattan distance)
+
     Refference - https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
     """
     print('Generating New Path')
-    start_node = Node(start)
-    end_node = Node(end)
-    open_list = [start_node]
-    closed_nodes = []
+    start_node = Node(start) # Initiate Start Node
+    end_node = Node(end) # Initiate End Node
+
+    # Check the if Start and End are iside the Map bound
+    if (start_node.location[1] >   global_map.shape[0]-1 or start_node.location[1] < 0 or start_node.location[0] > global_map.shape[1]-1 or start_node.location[0] < 0):
+        sys.exit('[ERROR] Start Location out of bound')
+    if (end_node.location[1] >   global_map.shape[0]-1 or end_node.location[1] < 0 or end_node.location[0] > global_map.shape[1]-1 or end_node.location[0] < 0):
+        sys.exit('[ERROR] End Location out of bound')
+
+    open_list = [start_node] # Initiate Open List
+    closed_nodes = [] #Initiate Closed List
     iterations = 0
 
     while open_list:
         current_node = open_list[0]
         index = 0
         iterations = iterations + 1
-        if(iterations>4000 and Type=='8c'):
+
+        # Try Algo with 4 Neighbours if iterations exceed 4k
+        if(iterations>4000 and Type=='8c'): 
             print('Trying with 4 Neibhours')
             return None
+
+        # Check if current Node cost is lowest
         for i, x in enumerate(open_list):
             if x.f_cost < current_node.f_cost:
                 current_node = x
@@ -204,59 +247,79 @@ def A_STAR(global_map, start, end, Type = '8c',e = 1, heuristic = 'eu' ):
         open_list.pop(index)
         closed_nodes.append(current_node)
 
+        # Check if goal node is reached
         if current_node == end_node:
             path = []
             node = current_node
             while node is not None:
                 path.append(node.location)
                 node = node.prev
+            # Return Back tracked path from end to goal
             return path[::-1]
 
 
         neibhours = []
+        # Index changes for 8 Neibhour method
         i_list = [0,0,-1,1,-1,-1,1,1]
         j_list = [-1,1,0,0,-1,1,-1,1]
+        # Index changes for 4 Neibhour method
         if(Type=='4c'):
             i_list = [0,0,1,-1]
             j_list = [1,-1,0,0]
 
-
+        # Check for all Neibhours of Current Node
         for k in range(len(i_list)):
 
             node_pos = [current_node.location[0]+i_list[k],current_node.location[1]+j_list[k]]
+            # Map Bound Check
             if (node_pos[1] >   global_map.shape[0]-1 or node_pos[1] < 0 or node_pos[0] > global_map.shape[1]-1 or node_pos[0] < 0):
                 continue
-
+            # Occupation in Grid Check
             if global_map[node_pos[1]][node_pos[0]] == 1:
                 continue
 
-            if(abs(Distance_compute([node_pos[0],node_pos[1]],current_node.location,'d') - 1.4143)<0.001 and 
-                global_map[node_pos[1]][node_pos[0]-1] == 1 and 
-                global_map[node_pos[1]-1][node_pos[0]] == 1):
+            # Diagonal/Corner Cutting Check
+            try:
+                # if(abs(Distance_compute([node_pos[0],node_pos[1]],current_node.location,'d') - 1.4143)<0.001 and 
+                #     global_map[node_pos[1]][node_pos[0]-1] == 1 and 
+                #     global_map[node_pos[1]-1][node_pos[0]] == 1):
+                #     continue
+                # if(abs(Distance_compute([node_pos[0],node_pos[1]],current_node.location,'d') - 1.4143)<0.001 and 
+                #     global_map[node_pos[1]][node_pos[0]+1] == 1 and 
+                #     global_map[node_pos[1]+1][node_pos[0]] == 1):
+                #     continue
+                if(abs(Distance_compute([node_pos[0],node_pos[1]],current_node.location,'d') - 1.4143)<0.001 and 
+                    global_map[current_node.location[1]+1][current_node.location[0]] == 1 and 
+                    global_map[current_node.location[1]][current_node.location[0]+1] == 1):
+                    continue
+                if(abs(Distance_compute([node_pos[0],node_pos[1]],current_node.location,'d') - 1.4143)<0.001 and 
+                    global_map[current_node.location[1]-1][current_node.location[0]] == 1 and 
+                    global_map[current_node.location[1]][current_node.location[0]-1] == 1):
+                    continue
+            except IndexError:
                 continue
-            # if(abs(Distance_compute([node_pos[0],node_pos[1]],current_node.location,'d') - 1.4143)<0.001 and 
-            #     global_map[node_pos[1]][node_pos[0]+1] == 1 and 
-            #     global_map[node_pos[1]+1][node_pos[0]] == 1):
-            #     continue
 
             neibhour_node = Node((node_pos[0], node_pos[1]))
-            neibhour_node.prev = current_node
-            neibhours.append(neibhour_node)
+            neibhour_node.prev = current_node # Update Neibhour Node Parent
+            neibhours.append(neibhour_node) # Add to other Neibhours
 
         for neibhour in neibhours:
             if neibhour in closed_nodes:
                 continue
-
+            # Update Costs of the Neibhour Nodes
             g = Distance_compute(neibhour.location,start_node.location,heuristic)
             h = Distance_compute(neibhour.location,end_node.location,heuristic)
             neibhour.update_cost(g,h*e)
             for onode in open_list:
                 if neibhour == onode and neibhour.g_cost > onode.g_cost:
                     continue
-                
+
             open_list.append(neibhour)
     
 def convert_path(path,trans,t):
+    '''
+    Translates and Rotates the given set of coordinates
+    '''
     npath = []
     for x in path:
         mat = [x[0],x[1]]
@@ -265,6 +328,9 @@ def convert_path(path,trans,t):
     return npath
 
 def Distance_compute(pos1,pos2,Type = 'd'):
+    '''
+    Distance Compute between two positions
+    '''
     x1 = pos1[0]
     y1 = pos1[1]
     x2 = pos2[0]
@@ -278,6 +344,9 @@ def Distance_compute(pos1,pos2,Type = 'd'):
         return abs(x1-x2)+abs(y1-y2)
 
 def rot2d(v,t):
+    '''
+    2D Rotation points
+    '''
     x,y = v[0],v[1]
     xr = x*cos(t)-y*sin(t)
     yr = x*sin(t)+y*cos(t)
@@ -286,6 +355,9 @@ def rot2d(v,t):
 ################### Motion Controll ########################
 
 def go_to_goal(goal):
+    '''
+    Function to command robot in ROS stage to go to given goal wrt /odom frame
+    '''
     global robot_rotation, robot_location
     d = Distance_compute(robot_location,goal)
     theta = robot_rotation[2]
@@ -295,10 +367,10 @@ def go_to_goal(goal):
     va = 0
     heading = math.atan2(goal[1]-robot_location[1],goal[0]-robot_location[0])
     err_theta = heading - theta
-    if(d>0.1):
+    if(d>0.01):
         vx = kl*abs(d)
         vx = 1
-    if(abs(err_theta)>0.1):
+    if(abs(err_theta)>0.01):
         va = ka*(err_theta)
 
     vel_1 = rospy.Publisher('/cmd_vel', geometry_msgs.msg.Twist,queue_size=10) # Publish Command to robot_1
@@ -308,6 +380,10 @@ def go_to_goal(goal):
     vel_1.publish(cmd)
 
 def Follow_path(path):
+    '''
+    Follows a given set of path
+    - Reaches all the points in a list in consecutive order
+    '''
     global final_goal_location, goal_reached
     cpath = path
     goal_point = cpath[-1]
@@ -324,24 +400,31 @@ def Follow_path(path):
 if __name__ == '__main__':
     rospy.init_node('A_STAR')
     rate = rospy.Rate(10.0)
+    # Subscribe to /odom and laser sensor
     sub = rospy.Subscriber('/base_scan', LaserScan, callback_laser) # Receive Laser Msg from /stage
     sub_odom = rospy.Subscriber('/odom', Odometry, callback_odom) # Receive Odom readings
     start = (int(robot_location[0]+9),int(robot_location[1]+10))
     final_goal_location = [rospy.get_param('/goalx'), rospy.get_param('/goaly'), 0]
     end = (int(final_goal_location[0]+9), int(final_goal_location[1]+10))
+    global_map[13][8] = 1 
+
+    # A star factors
     neibhour_type = '8c'
     heuristic = 'eu'
-    global_map[13][8] = 1 
     heuristic_factor = 10
+    
+    # Get the Path in Map coordinates
     final_path = A_STAR(global_map[::-1],start,end,neibhour_type,heuristic_factor,heuristic)
     if(final_path==None):
         neibhour_type = '4c'
         final_path = A_STAR(global_map[::-1],start,end,neibhour_type,heuristic_factor,heuristic)
     odom_trans = [-9,-10]
+    # Convert points in Map coordinate to Global Coordinates
     path_odom_frame = convert_path(final_path,odom_trans,0)
     goal_reached = False
     
     while not rospy.is_shutdown():
+        # Accuire new path when the Goal parameters are changed
         if(final_goal_location != [rospy.get_param('/goalx'), rospy.get_param('/goaly'), 0]):
             final_goal_location = [rospy.get_param('/goalx'), rospy.get_param('/goaly'), 0]
             start = (int(robot_location[0]+9),int(robot_location[1]+10))
@@ -350,13 +433,14 @@ if __name__ == '__main__':
             final_path = A_STAR(global_map[::-1],start,end,neibhour_type,heuristic_factor,heuristic)
             path_odom_frame = convert_path(final_path,odom_trans,0)
 
+        # Visualize the Path in RVIZ
         points_publisher(path_odom_frame)
         goal_location_marker(final_goal_location)
         if(goal_reached==False):
+            # Follow the path till goal is reached
             Follow_path(path_odom_frame)
         elif(goal_reached==True):
             print('Goal Reached')
-
 
 
 
